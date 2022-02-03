f99776df50a3724d557bce28f6de85c6404e5b6b
brightsdays <brightsdayss@gmail.com>
2022-02-03 17:11:49 +0300

    add ui-details

---
+++ src/components/ui/uiDetails.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="details">
+    <div
+        :class="['details__summary', {'details__summary--open': isOpen}]"
+        @click="toggleDetails()"
+    >
+      {{ title }}
+    </div>
+    <transition name="fade">
+      <div v-if="isOpen" class="details__content">
+        <slot />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  title: {type: String, default: 'Title'}
+})
+let isOpen = ref(true)
+
+const toggleDetails = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<style lang="scss">
+.details {
+  padding: 15px;
+  border-top: 1px solid var(--summary);
+  border-bottom: 1px solid var(--summary);
+  margin-top: -1px;
+
+  &__summary {
+    position: relative;
+    list-style: none;
+    font-size: 2.2em;
+    font-weight: 500;
+    cursor: pointer;
+    transition: padding 0.3s;
+
+    &:before, &:after {
+      content: '';
+      position: absolute;
+      border-top: 2px solid var(--main);
+      transition: transform 0.4s;
+    }
+
+    &:before {
+      top: 12px;
+      right: 0;
+      width: 15px;
+      transform: rotate(-40deg);
+    }
+
+    &:after {
+      top: 12px;
+      right: 11px;
+      width: 15px;
+      transform: rotate(40deg);
+    }
+
+    &--open {
+      &:before {
+        transform: rotate(40deg);
+      }
+
+      &:after {
+        transform: rotate(-40deg);
+      }
+    }
+  }
+}
+</style>
