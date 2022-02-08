a2caa567033638dbcaccda418202e3d4eb6c01ab
brightsdays <brightsdayss@gmail.com>
2022-02-08 17:48:18 +0300

    add dice roll

---
+++ src/plugins/rollDices.ts
@@ -0,0 +1,13 @@
+export default (dices: number, facets: number, remove: string | null) => {
+    let result = [];
+
+    for (let i = 0; i < dices; i++) {
+        const roll = Math.floor( Math.random() * facets ) + 1
+        result.push(roll)
+    }
+
+    if (remove === 'min') result.sort().shift()
+    if (remove === 'max') result.sort().pop()
+
+    return result.reduce((a, b) => a + b, 0)
+}
