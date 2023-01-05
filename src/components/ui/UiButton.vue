46ddb12c1314aad5730fe320b43fd34e0f6dc3b6
vivanov <brightsdayss@gmail.com>
2023-01-05 17:02:06 +0400

    add ui components

---
+++ src/components/ui/UiButton.vue
@@ -0,0 +1,47 @@
+<template>
+  <button
+    class="button"
+    :class="type"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const {
+  text,
+  type
+} = defineProps<{
+  text: string
+  type: 'simple' | 'big'
+}>()
+</script>
+
+<style lang="scss" scoped>
+.button {
+  color: var(--main);
+  cursor: pointer;
+  &:disabled, &[disabled] {
+    color: var(--second);
+    pointer-events: none;
+  }
+
+  &.simple {
+    padding: 5px 10px;
+    font-family: "Ubuntu", sans-serif;
+    font-size: 1.3em;
+    border: 2px solid var(--main);
+    border-radius: 15px;
+    cursor: pointer;
+    &:hover {
+      background: var(--second-background);
+    }
+  }
+
+  &.big {
+    font-family: "Pirata One", sans-serif;
+    font-size: 3.2em;
+    border: none;
+  }
+}
+</style>
---
+++ src/components/ui/UiSelect.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="select">
+    <label class="label">{{ label }}</label>
+    <select
+      class="input"
+      def
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+      <option
+        v-for="option in options"
+        :key="option"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {
+  label,
+  options,
+  modelValue
+} = defineProps<{
+  label: string
+  options: string[]
+  modelValue: string
+}>()
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<style lang="scss" scoped>
+.select {
+  position: relative;
+  &::after {
+      content: '⌄';
+      position: absolute;
+      top: 0;
+      right: 0;
+      font-size: 2.6em;
+      color: var(--main);
+    }
+
+  .input {
+    position: relative;
+    margin: -2px 0 0 10px;
+    font-family: "Pirata One", sans-serif;
+    font-size: 3em;
+    padding-right: 20px;
+    color: var(--main);
+    border: none;
+    outline: none;
+    cursor: pointer;
+    appearance: none;
+
+    option {
+      font-size: 1em;
+      direction: rtl;
+    }
+  }
+  .label {
+    font-family: "Pirata One", sans-serif;
+    font-size: 3.2em;
+    color: var(--second);
+  }
+}
+</style>
